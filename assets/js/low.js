a83ad083db83dc3fd2d2974a9f498155c8fb7bb6
yumimityan <58584054+yumimityan@users.noreply.github.com>
2021-09-21 13:30:58 +0900

    Update low.js

--- assets/js/low.js
+++ assets/js/low.js
@@ -2,3 +2,13 @@ window.onload = function () {
     const spinner = document.getElementById('loading');
     spinner.classList.add('loaded');
 }
+
+$(function () {
+    if (localStorage.getItem('memo')) {
+        $('#te').val(localStorage.getItem('memo'));
+    }
+    $('#te').bind('keyup', function () {
+        localStorage.setItem('memo', $('#te').val());
+    });
+
+});
